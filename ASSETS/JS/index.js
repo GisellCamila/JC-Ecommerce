44e53731c581d1479eced7c0c152f747962b58f1
GisellCamila <sfkl>
2023-11-07 22:17:54 -0500

    Se creo menu responsive

---
+++ ASSETS/JS/index.js
@@ -0,0 +1,29 @@
+
+
+const header = document.getElementById('header')
+const menuOpen = document.getElementById('menu-open')
+const menuClose = document.getElementById('menu-close')
+const navMenu = document.getElementById('nav-menu')
+
+
+// Menu responsive
+if (menuOpen) {
+    menuOpen.addEventListener('click', () => {
+        navMenu.classList.add('header__nav--visible')
+    })
+}
+
+if (menuClose) {
+    menuClose.addEventListener('click', () => {
+        navMenu.classList.remove('header__nav--visible')
+    })
+}
+
+// Scroll in menu
+addEventListener('scroll', () => {
+    if (this.screenY > 150) {
+        header.classList.add('header--fill')
+    } else {
+        header.classList.remove('header--fill')
+    }
+})
